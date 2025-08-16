69f0456e92ea565745c017b083dcb118030e3fef
Sourav Choudhary <143872838+Onethybeing@users.noreply.github.com>
2025-08-16 18:06:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,27 @@ import streamlit as st
 import cv2
 import numpy as np
 from model import load_model, predict
-from detectron2.utils.visualizer import Visualizer
+from detectron2.utils.visualizer import Visualizer, ColorMode
 from detectron2.data import MetadataCatalog
+from dataset_registration import register_datasets
 
 st.title("Custom Object Detection with Detectron2")
+
+# Register datasets + metadata
+register_datasets()
+metadata = MetadataCatalog.get("hardhat_val")
 
 # Load model
 predictor = load_model("best.pth", "config.yaml")
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
 
     outputs = predict(image, predictor)
 
-    v = Visualizer(image[:, :, ::-1], MetadataCatalog.get("__unused__"), scale=1.2)
+    v = Visualizer(image[:, :, ::-1], metadata, scale=1.2, instance_mode=ColorMode.IMAGE_BW)
     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 
     st.image(out.get_image()[:, :, ::-1], caption="Detected Objects")
