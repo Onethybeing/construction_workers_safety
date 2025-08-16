e042d3d7c98b5c6d5f416acc7e974fcf882c3eaa
Sourav Choudhary <143872838+Onethybeing@users.noreply.github.com>
2025-08-16 18:08:03 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import torch
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
-import cv2
 import os
 import gdown
 
@@ -10,7 +9,6 @@ import gdown
 WEIGHTS_PATH = "best.pth"
 
 def download_weights(file_id, output_path):
-    # Check if the file already exists
     if not os.path.exists(output_path):
         print("Downloading model weights...")
         url = f"https://drive.google.com/uc?id={file_id}"
@@ -20,10 +18,10 @@ def download_weights(file_id, output_path):
         print("Model weights already exist.")
 
 def load_model(weights_path=WEIGHTS_PATH, config_path=None):
-    # This is the ID of your file in Google Drive
+    # Google Drive file ID for your weights
     google_drive_file_id = "1jE-h5dHHYuyxBYBgseiQbg5es4nnRb5s"
     
-    # Download the weights file before loading the model
+    # Ensure weights exist
     download_weights(google_drive_file_id, weights_path)
     
     cfg = get_cfg()
@@ -31,12 +29,16 @@ def load_model(weights_path=WEIGHTS_PATH, config_path=None):
         cfg.merge_from_file(config_path)
     else:
         cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # change based on your dataset
+    
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # ["Helmet", "No Helmet"]
     cfg.MODEL.WEIGHTS = weights_path
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    
+    # Ensure CPU works if CUDA is not available
+    cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    
     predictor = DefaultPredictor(cfg)
     return predictor
 
 def predict(image, predictor):
-    outputs = predictor(image)
-    return outputs
+    return predictor(image)
